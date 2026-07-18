ae88ba2134305993c783f31fa1186b09bb0cc1cb
Jacob Arén <jacob.aren.1@gmail.com>
2026-07-18 22:22:58 +0200

    BT-2: Added docstring for treenode

--- tree_node.py
+++ tree_node.py
@@ -1,6 +1,27 @@
 import unittest
 
 class TreeNode(object):
+    """
+    Represents a node in a binary tree.
+
+    Parameters
+    ----------
+    key : Any
+        The comparison key used to order the node in the tree.
+    value : Any, optional
+        The stored value associated with the node.
+
+    Attributes
+    ----------
+    key : Any
+        The node's ordering key.
+    value : Any, optional
+        The value stored in the node.
+    left : TreeNode or None
+        Reference to the left child node.
+    right : TreeNode or None
+        Reference to the right child node.
+    """
 
     def __init__(self, key: ..., value:... = None):
         self.key = key
