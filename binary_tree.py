172ac7bdbabd5e61a070c8c49da4900d09659611
Jacob Arén <jacob.aren.1@gmail.com>
2026-07-18 23:21:56 +0200

    BT-3: Created put_func

--- binary_tree.py
+++ binary_tree.py
@@ -15,7 +15,23 @@ class BinaryTree(object):
                 f"A node in a binary tree must be a TreeNode, got {type(node)}"
             )
         
-        self.put_func(node)
+        self.root = self.put_func(node, self.root)
     
-    def put_func(self, node):
-        pass
+    def put_func(self, node: TreeNode, ref_node: TreeNode | None):
+
+        if ref_node is None:
+            ref_node = node
+            return ref_node
+
+        elif node < ref_node:
+
+            ref_node.left = self.put_func(node, ref_node.left)
+            return ref_node
+        
+        elif node > ref_node:
+            ref_node.right = self.put_func(node, ref_node.right)
+            return ref_node
+        
+        else:
+            print("Node already exists:", ref_node)
+            return ref_node
