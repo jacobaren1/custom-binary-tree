e551e3e69436cc77581ba00b17eb497c58355a96
Jacob Arén <jacob.aren.1@gmail.com>
2026-07-19 00:06:33 +0200

    BT-3: Created unittest for binary tree

--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -6,16 +6,39 @@ from tree_node import TreeNode
 from binary_tree import BinaryTree, BinTreeError
 
 
-class TestBinaryTreePut(unittest.TestCase):
-    bt = BinaryTree()
-    
+class TestBinaryTree(unittest.TestCase):
+    def setUp(self):
+        self.bt = BinaryTree()
+
     def test_put_rejects_non_tree_node(self):
         with self.assertRaises(BinTreeError):
             self.bt.put("a")
 
-    def test_put_accepts_tree_node(self):
-        bt.put( TreeNode("a") )
-        self.assertIsNotNone(bt.root)
+    def test_put_inserts_nodes_into_binary_search_tree_order(self):
+        self.bt.put(TreeNode("b"))
+        self.bt.put(TreeNode("c"))
+        self.bt.put(TreeNode("a"))
+
+        self.assertEqual(self.bt.root.key, "b")
+        self.assertEqual(self.bt.root.left.key, "a")
+        self.assertEqual(self.bt.root.right.key, "c")
+
+    def test_exists_returns_true_for_existing_node(self):
+        self.bt.put(TreeNode("b"))
+        self.bt.put(TreeNode("c"))
+        self.bt.put(TreeNode("a"))
+
+        self.assertTrue(self.bt.exists(TreeNode("a", 1)))
+        self.assertTrue(self.bt.exists(TreeNode("b", 2)))
+        self.assertTrue(self.bt.exists(TreeNode("c", 3)))
+
+    def test_exists_returns_false_for_missing_node(self):
+        self.bt.put(TreeNode("b"))
+        self.bt.put(TreeNode("c"))
+        self.bt.put(TreeNode("a"))
+
+        self.assertFalse(self.bt.exists(TreeNode("d")))
+
 
 if __name__ == "__main__":
     unittest.main()
